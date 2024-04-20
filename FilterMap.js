6c0effea9b8b31cb6126f31f16ea39c6e4f31c8b
JeneshRaj <jeneshudash12@gmail.com>
2024-04-20 17:05:53 +0545

    map practise code

--- FilterMap.js
+++ FilterMap.js
@@ -83,8 +83,8 @@
 
 
 //********** Reduce *****************/
-const myNums = [1,2,3,4,5];
-const initialValues = 0;
+// const myNums = [1,2,3,4,5];
+// const initialValues = 0;
 
 //using Fucntions 
 // const myTotal = myNums.reduce(function (acc,curVal){
@@ -100,27 +100,69 @@ const initialValues = 0;
 
 
 ///Reduce with objects
-const shoppingCart = [
-    {
-        itemName : "js course",
-        price : 200
-    },
-    {
-        itemName : "python course",
-        price : 400
-    },
-    {
-        itemName : "java course",
-        price : 700
-    },
-    {
-        itemName : "cpp course",
-        price : 1000
-    }
-]
+// const shoppingCart = [
+//     {
+//         itemName : "js course",
+//         price : 200
+//     },
+//     {
+//         itemName : "python course",
+//         price : 400
+//     },
+//     {
+//         itemName : "java course",
+//         price : 700
+//     },
+//     {
+//         itemName : "cpp course",
+//         price : 1000
+//     }
+// ]
 
-const myCart = shoppingCart.reduce((acc,item) => {
-   return acc + item.price
+// const myCart = shoppingCart.reduce((acc,item) => {
+//    return acc + item.price
 
-},0)
-console.log(myCart)
+// },0)
+// console.log(myCart)
+
+
+
+// const myArr = [2,3,4,5,6,7,8];
+
+// function doubleArr () {
+//  myArr.map((items)=>{
+//     return items * 2;
+//   })
+// }
+// doubleArr(myArr)
+
+
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10];
+// const  addNums = myNums.map ((items)=>{
+//     return items + 10;
+// })
+// console.log(addNums)
+
+// 1) Make an array of numbers that are doubles of the first array
+// function doubleArr (arr){    
+// const doubleNums = arr.map((items) => {
+//     return items *2;
+// })
+// console.log(doubleNums)
+// }
+// doubleArr([2,2,2,2,2])
+// doubleArr([3,3,3,3,3])
+// doubleArr([4,4,4,4,4])
+// // doubleArr()
+
+
+// 2) Take an array of numbers and make them strings
+// function stringItUp(arr) {
+//     const strArr = arr.map((items)=> {
+//         return items.toString()
+//     })
+//     console.log(strArr)
+// }
+
+// stringItUp([2,3,4,5])
