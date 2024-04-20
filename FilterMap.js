93fa637548d3357021de64933457106e73ab3dc6
JeneshRaj <jeneshudash12@gmail.com>
2024-04-20 17:34:33 +0545

    map with objects

--- FilterMap.js
+++ FilterMap.js
@@ -175,3 +175,75 @@
 //     console.log(myArr)
 // }
 // capitalizeNames(["jenesh","puja","ram", "jeebesh","shrinika"]);
+
+// 4) Make an array of strings of the names
+// function namesOnly(arr){
+//     // your code here
+//     const str = arr.map((items) => {
+//         return items.name
+//     })
+//     console.log(str)
+//   }
+  
+//   console.log(namesOnly([
+//       {
+//           name: "Angelina Jolie",
+//           age: 80
+//       },
+//       {
+//           name: "Eric Jones",
+//           age: 2
+//       },
+//       {
+//           name: "Paris Hilton",
+//           age: 5
+//       },
+//       {
+//           name: "Kayne West",
+//           age: 16
+//       },
+//       {
+//           name: "Bob Ziroll",
+//           age: 100
+//       }
+//   ])); 
+
+
+
+
+function makeStrings(arr){
+    // your code here
+
+    const str  = arr.map((items) => {
+       if (items.age >= 18){
+        return `${items.name} you can go to matrix`
+       }else {
+        return `${items.name} your under age`
+       }
+    })
+    console.log(str)
+  }
+  
+  console.log(makeStrings([
+      {
+          name: "Angelina Jolie",
+          age: 80
+      },
+      {
+          name: "Eric Jones",
+          age: 2
+      },
+      {
+          name: "Paris Hilton",
+          age: 5
+      },
+      {
+          name: "Kayne West",
+          age: 16
+      },
+      {
+          name: "Bob Ziroll",
+          age: 100
+      }
+  ])); 
+
